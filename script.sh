7e240ab4aeb6902df38d4878f248117cb4a7ccdb
rajenrathore <rajendra.rathore.kiit@gmail.com>
2024-10-26 23:01:27 +0530

    change

--- script.sh
+++ script.sh
@@ -11,8 +11,19 @@ docker push rajendrarathore/app2:latest
 # Apply Kubernetes manifests
 #kubectl apply -f k8s/app1-deployment.yaml
 #kubectl apply -f k8s/app2-deployment.yaml
-helm install app1 ./app1-chart
-helm install app2 ./app2-chart
+if helm ls --all --short | grep -q app1; then
+  echo "Helm release app1 already existing. skipping install"
+else
+  echo "installing app1"
+  helm install app1 ./app1-chart
+fi
+
+if helm ls --all --short | grep -q app2; then
+  echo "Helm release app2 already existing. skipping install"
+else
+  echo "installing app2"
+  helm install app2 ./app2-chart
+fi
 # Wait for deployments to be ready
 kubectl rollout status deployment/app1
 kubectl rollout status deployment/app2
